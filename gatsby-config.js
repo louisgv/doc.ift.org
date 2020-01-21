aa2610a86689495c8561c4dfc024afa231669fa6
Karlos Santos <lgvichy@gmail.com>
2020-01-20 21:00:17 -0800

    Proper status bar config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,40 @@
-const themeOptions = require('gatsby-theme-apollo-docs/theme-options');
+const themeOptions = require("./src/theme-options")
 
 module.exports = {
-  plugins: [  
+  pathPrefix: "/doc.ift.org",
+  siteMetadata: {
+    title: 'IFT Docs'
+  },
+  plugins: [
     `gatsby-plugin-dark-mode`,
     `gatsby-plugin-offline`,
     {
-      resolve: 'gatsby-theme-apollo-docs',
+      resolve: "gatsby-theme-apollo-docs",
       options: {
         ...themeOptions,
+        
         root: __dirname,
-        siteName: "doc.ift",
-        subtitle: "IFT Documentations",
-        description: "A guide to IFT's standards and APIs",
         githubRepo: "louisgv/doc.ift.org",
         sidebarCategories: {
-          null: ["index", "getting-started", "whats-new"],
-          Features: [
-            "features/mocking",
-            "features/errors",
-            "features/data-sources",
+          null: ["index", "intro/feedback"],
+          "Background Information": [
+            "part1/1-gdst-traceability-overview",
+            "part1/2-traceability-and-standards",
+            "part1/3-traceability-data-systems-sharing",
           ],
+          "GDST Framework": [
+            "part2/4-data-req-wild-caught",
+            "part2/5-data-req-aquaculture",
+            "part2/6-obj-loc-req",
+            "part2/7-interoperability-wild-farm",
+            "part2/8-traceability-data-use-cases",
+          ],
+
+          "Basic Universal Lists": [
+            "part3/appendix-explanation",
+            "part3/appendix-1",
+            "part3/appendix-2",
+          ]
         },
       },
     },
